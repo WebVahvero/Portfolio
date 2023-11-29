a7ceab5e02f50fd48d79d7b07ab56d681791744c
Kurunsaari Valtteri <valtteri.kurunsaari@attido.com>
2023-11-29 15:19:55 +0200

    Changed to Prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInjector = require('html-webpack-injector');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: {
         index: './src/index.js',
     },
